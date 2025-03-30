4d4ca5f4da729993269f17af128d1a14e2d13315
stephenmhuli <smhuli@d-tree.org>
2025-03-30 22:06:12 +0300

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Global Network</title>
+</head>
+<body>
+    <h1>Welcome to the Global Network</h1>
+    <p>Click the button below to view the list of Global Networks</p>
+
+    <a href="/" class="btn">
+        Find networks
+    </a>
+</body>
+</html>
